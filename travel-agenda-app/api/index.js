9a3c9770d895791eeb846f4e1871e92bdc4e3f14
pvilxx <pau.vilardell@mataro.epiaedu.cat>
2025-05-27 17:25:59 +0200

    fix: compatibilidad Vercel usando serverless-http

--- travel-agenda-app/api/index.js
+++ travel-agenda-app/api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const agendaRoutes = require('../src/routes/agendaRoutes');
+const serverless = require('serverless-http');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', agendaRoutes);
 
-module.exports = app;
+module.exports = serverless(app);
